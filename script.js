57a4310607e80109ff267c9cf6af9d30f8a0375c
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-10-21 15:15:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+function confirmBox(){
+	let confirmDialogBox=confirm("Do you want to proceed");
+
+	if(confirmDialogBox){
+		alert("You clicked OK. Proceeding...")
+	}else{
+		alert("You clicked Cancel. Exiting...")
+	}
+}
+
+confirmBox()
+
